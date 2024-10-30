3a09b69345b4b14651af83b0bb62099ab66d4c27
amany091 <amanyzeyada10@gmail.com>
2024-10-30 17:27:18 +0300

    fix token issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs")
 const asyncWrapper = require("../utils/asyncWrapper")
 const ApiError = require("../utils/ApiError")
 
-const createToken = (payload)=>{
+const createToken = (payload) => {
      return jwt.sign({userId:payload} , process.env.JWT_SECRET_KEY , {expiresIn:process.env.JWT_EXPIRE_TIME})
     
 }
@@ -30,24 +30,24 @@ exports.login = asyncWrapper(async(req,res,next)=>{
     if(!isMatch) return next(new ApiError(process.env.INVALID_CREDENTIALS,401))
     const token =  createToken(user._id)
     res.cookie("access_token", `Bearer ${token}`, {
-        httpOnly: true, // Ensures the cookie is not accessible by JavaScript
-        // secure: false, // Only set to true in production (HTTPS)
-        // sameSite: "None", // Required for cross-site cookies (e.g., different domain or ports)
-        withCredentials: true, // Include cookies in requests
-        // maxAge: 7 * 24 * 60 * 60 * 1000, // Set cookie expiration time (1 week)
+        httpOnly: true, 
+        secure: false, 
+        sameSite: "lax", 
+        withCredentials: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    return res.status(200).json({data : user})
+    return res.status(200).json({data : {user: user, token : token}})
 
 })
 
 
 exports.logoutCtrl = asyncWrapper(async (req, res) => {
     res.clearCookie("access_token");
-    res.send({ success: true });
+    await res.send({ success: true });
 }),
 
 exports.getCurrentUserCtrl= asyncWrapper(async (req, res) => {
     const user = await User.findOne({ _id: req.user._id });
     if (!user) return res.status(404).json({ success: false, error: "user not found." })
-    res.status(200).json({ success: true, data: user });
+    await res.status(200).json({ success: true, data: user });
 })
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const ApiError = require("../utils/ApiError");
 const asyncWrapper = require("../utils/asyncWrapper");
 const User = require("../models/user");
+
 exports.authentication = asyncWrapper(async (req, res, next) => {
   let token;
   if (
@@ -9,13 +10,9 @@ exports.authentication = asyncWrapper(async (req, res, next) => {
     req.cookies.access_token.startsWith("Bearer")
   ) {
     token = req.cookies.access_token.split(" ")[1];
-    console.log(token);
   }
   if (!token)
-    return next(
-      new ApiError("You are not login , please login to access this route..."),
-      401
-    );
+    return next(new ApiError("You are not logged in, please login to access this route...", 401));
   const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
   // check if user exist
@@ -28,13 +25,10 @@ exports.authentication = asyncWrapper(async (req, res, next) => {
       401
     );
   req.user = currentUser;
-  console.log(req.user)
-  console.log("currentUser", currentUser)
   next();
 })
 
 exports.allowedTo = (...roles) => {
-    console.log(roles);
   return asyncWrapper(async (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(
